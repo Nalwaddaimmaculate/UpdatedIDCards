203af7d75b5a1d7aab11b47aff414b718157b741
Nalwaddaimmaculate <117144141+Nalwaddaimmaculate@users.noreply.github.com>
2023-07-14 12:25:27 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Idcard from './Idcard';
+import Idcards from './Idcards';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Welcome to IDCards App</h1>
+      {/*<Idcard/> */}
+      <Idcards/>
+
+     
+    </div>
+  );
+}
+
+export default Idcards;
